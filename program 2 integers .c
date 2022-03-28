5c489020907ec1845b12bf126ecc113644442820
Elsie-kay <101639829+Elsie-kay@users.noreply.github.com>
2022-03-28 17:56:09 +0300

    Add files via upload

---
+++ program 2 integers .c
@@ -0,0 +1,13 @@
+//a program that accepts two integers from the user and calculate the product of the two integers
+#include<stdio.h>
+int main()
+{
+int e,l,s;
+printf("12 \n:");
+scanf("%d",&e);
+printf("13 \n:");
+scanf("%d",&l);
+s=e*l;
+printf("the product is=%d",s);
+return 0;
+}
